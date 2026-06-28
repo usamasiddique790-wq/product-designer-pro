fbfbf98f2a687bfe854fb18baedc408640a23f35
Usama Siddique <usamasiddique790@gmail.com>
2026-06-28 10:25:25 +0500

    Load WooCommerce product image in designer canvas

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -3,7 +3,6 @@ const PDPCanvas = {
 
   init() {
     const product = PDPProductManager.getCurrent();
-
     this.designArea = product.designArea;
 
     const canvas = new fabric.Canvas("pdp-canvas", {
@@ -14,20 +13,64 @@ const PDPCanvas = {
     canvas.setWidth(product.canvas.width);
     canvas.setHeight(product.canvas.height);
 
-    PDPTemplates.addProductTemplate(canvas, product);
+    this.loadProductImage(canvas, product);
     this.bindBoundaries(canvas);
 
     return canvas;
   },
 
+  loadProductImage(canvas, product) {
+    const context = PDPProductContext.get();
+
+    if (!context.image) {
+      PDPTemplates.addProductTemplate(canvas, product);
+      return;
+    }
+
+    fabric.Image.fromURL(
+      context.image,
+      (img) => {
+        const canvasWidth = canvas.getWidth();
+        const canvasHeight = canvas.getHeight();
+
+        const scale = Math.min(
+          (canvasWidth * 0.8) / img.width,
+          (canvasHeight * 0.85) / img.height
+        );
+
+        img.set({
+          left: canvasWidth / 2,
+          top: canvasHeight / 2,
+          originX: "center",
+          originY: "center",
+          scaleX: scale,
+          scaleY: scale,
+          selectable: false,
+          evented: false,
+          excludeFromExport: true,
+          pdpProductImage: true,
+        });
+
+        canvas.add(img);
+        canvas.sendToBack(img);
+        canvas.renderAll();
+      },
+      {
+        crossOrigin: "anonymous",
+      }
+    );
+  },
+
   buildTemplate(canvas) {
     const product = PDPProductManager.getCurrent();
 
-    const oldTemplateObjects = canvas.getObjects().filter((obj) => obj.excludeFromExport === true);
+    const oldTemplateObjects = canvas
+      .getObjects()
+      .filter((obj) => obj.excludeFromExport === true);
 
     oldTemplateObjects.forEach((obj) => canvas.remove(obj));
 
-    PDPTemplates.addProductTemplate(canvas, product);
+    this.loadProductImage(canvas, product);
     canvas.renderAll();
   },
 
